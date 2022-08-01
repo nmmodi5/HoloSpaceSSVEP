f8448a222c81420341fd09b07bd2ac67b7db85e1
Griffin W. Milsap <griffin.milsap@jhuapl.edu>
2022-08-01 17:07:50 -0400

    progress on training module encapsulation

--- hololight/shallowfbcsptraining.py
+++ hololight/shallowfbcsptraining.py
@@ -1,3 +1,5 @@
+import asyncio
+from pathlib import Path
 from dataclasses import field, replace
 
 import io
@@ -5,6 +7,7 @@ import logging
 import json
 
 import ezmsg.core as ez
+from ezmsg.eeg.eegmessage import EEGMessage
 from ezmsg.util.messagelogger import MessageDecoder
 
 import numpy as np
@@ -12,16 +15,33 @@ import numpy as np
 import torch as th
 from torch.utils.data import Dataset, ConcatDataset
 
-from .shallowfbcspnet import ShallowFBCSPNet, ShallowFBCSPParameters
 from .sampler import SampleMessage
+from .classdecodemessage import ClassDecodeMessage
+from .shallowfbcspnet import (
+    EpochInfo, 
+    ShallowFBCSPCheckpoint,
+    ShallowFBCSPNet, 
+    ShallowFBCSPParameters,
+    ShallowFBCSPTraining,
+    ShallowFBCSPTrainingParameters,
+    balance_dataset
+)
 
-from typing import AsyncGenerator, List, Optional, Tuple, Any
+from typing import AsyncGenerator, List, Optional, Tuple, Any, Set
 
 logger = logging.getLogger( __name__ )
 
 class FBCSPDataset( Dataset ):
     """
     Lazy loaded dataset from disk
+    All messages in file assumed to be SampleMessages
+    All SampleMessages assumed to have same sample dimensions and fs
+
+    It'd probably be better to pickle SampleMessages directly to disk
+    and reconstitute them to avoid issues with file format between runs,
+    but a part of me worries the pickle format is not very forward-looking
+    and the json serialized format may be usable by other programs and
+    analyses in the future... -Griff
     """
     file: io.TextIOWrapper
 
@@ -30,13 +50,18 @@ class FBCSPDataset( Dataset ):
 
     dtype: np.dtype
 
+    fs: Optional[ float ] = None
+    n_ch: Optional[ int ] = None
+    n_time: Optional[ int ] = None
+
     def __init__( self, filename: Path, single_precision: bool = True ) -> None:
         super().__init__()
 
         self.dtype = np.float32 if single_precision else np.float64
         self.seekpoints = list()
         self.file = open( filename, 'r' )   
-        self.labels = []     
+        self.labels = []
+  
         while self.file.readable():
             seekpoint = self.file.tell()
             line = self.file.readline()
@@ -44,6 +69,10 @@ class FBCSPDataset( Dataset ):
                 self.seekpoints.append( seekpoint )
                 obj = json.loads( line, cls = MessageDecoder )
                 self.labels.append( int( obj[ 'trigger' ][ 'value' ] ) )
+                self.fs = float( obj[ 'sample' ][ 'fs' ] )
+                data: np.ndarray = obj[ 'sample' ][ 'data' ]
+                self.n_ch = data.shape[ obj[ 'sample' ][ 'ch_dim' ] ]
+                self.n_time = data.shape[ obj[ 'sample' ][ 'time_dim' ] ]
             else: break
 
     def __len__( self ) -> int:
@@ -52,21 +81,116 @@ class FBCSPDataset( Dataset ):
     def __getitem__( self, idx: int ) -> Tuple[ th.Tensor, th.Tensor ]:
         self.file.seek( self.seekpoints[ idx ] )
         obj = json.loads( self.file.readline(), cls = MessageDecoder )
-        data = np.transpose( obj[ 'sample' ][ 'data' ] ).astype( self.dtype )
+        time_dim: int = obj[ 'sample' ][ 'time_dim' ]
+        data: np.ndarray = obj[ 'sample' ][ 'data' ]
+        data = np.swapaxes( data.astype( self.dtype ), time_dim, 0 )
         return th.tensor( data ), th.tensor( self.labels[ idx ] )
 
     def __del__( self ):
         self.file.close()
 
-class ShallowFBCSPTrainingSettings( ez.Settings ):
-    session_dir: Path
+class FBCSPTrainMessage( ez.Message ):
+    n_epochs: int
+    reset: bool = False
 
-class ShallowFBCSPTrainingState( ez.State ):
+class FBCSPTrainingSettings( ez.Settings ):
+    model_dir: Path
+
+class FBCSPTrainingState( ez.State ):
     ...
 
-class ShallowFBCSPTraining( ez.Unit ):
-    SETTINGS: ShallowFBCSPTrainingSettings
-    STATE: ShallowFBCSPTrainingState
+class FBCSPTraining( ez.Unit ):
+    SETTINGS: FBCSPTrainingSettings
+    STATE: FBCSPTrainingState
+
+    INPUT_TRAIN = ez.InputStream( FBCSPTrainMessage )
+    OUTPUT_CHECKPOINT = ez.OutputStream( ShallowFBCSPCheckpoint )
+    OUTPUT_EPOCH = ez.OutputStream( EpochInfo )
+
+    @ez.subscriber( INPUT_TRAIN )
+    @ez.publisher( OUTPUT_CHECKPOINT )
+    @ez.publisher( OUTPUT_EPOCH )
+    async def train( self, msg: Path ) -> AsyncGenerator:
+
+        # Load datasets
+        datasets: List[ FBCSPDataset ] = list()
+        all_labels: List[ int ] = list()
+        for fname in msg.glob( '*.txt' ):
+            dataset = FBCSPDataset( fname )
+            if len( dataset ) == 0: continue
+            datasets.append( dataset )
+            in_chans = dataset.n_ch
+            in_time = dataset.n_time
+            fs = dataset.fs
+            all_labels = all_labels + dataset.labels
+        datasets = ConcatDataset( datasets )
+
+        # Create network
+        net = ShallowFBCSPNet( 
+            ShallowFBCSPParameters(
+                in_chans = in_chans,
+                n_classes = len( set( all_labels ) ),
+                input_time_length = in_time,
+                single_precision = True,
+                cropped_training = True,
+                filter_time_length = int( 0.1 * fs ),
+                pool_time_length = int( 0.3 * fs ),
+                pool_time_stride = int( 0.06 * fs )
+            )
+        )
+
+        # Create training module
+        num_epochs = 30
+        train, test = balance_dataset( datasets, ( 0.8, 0.2 ) )
+        training = ShallowFBCSPTraining(
+            net.model, 
+            train, 
+            test,
+            ShallowFBCSPTrainingParameters(
+                annealing_epochs = num_epochs
+            )
+        )
+
+        # Train over many epochs and publish epoch info
+        loop = asyncio.get_running_loop()
+        for _ in range( num_epochs ):
+            info = await loop.run_in_executor( None, training.run_epoch )
+            yield self.OUTPUT_EPOCH, info
+
+        # Publish Checkpoint
+        yield self.OUTPUT_CHECKPOINT, net.checkpoint
+
+class FBCSPInferenceSettings( ez.Settings ):
+    checkpoint_file: Optional[ Path ] = None
+    inference_device: str = 'cpu'
+
+class FBCSPInferenceState( ez.Settings ):
+    net: Optional[ ShallowFBCSPNet ] = None
+
+class FBCSPInference( ez.Unit ):
+
+    SETTINGS: FBCSPInferenceSettings
+    STATE: FBCSPInferenceState
+
+    INPUT_MODEL = ez.InputStream( ShallowFBCSPCheckpoint )
+
+    INPUT_SIGNAL = ez.InputStream( EEGMessage )
+    OUTPUT_DECODE = ez.OutputStream( ClassDecodeMessage )
+
+    @ez.subscriber( INPUT_MODEL )
+    async def on_checkpoint( self, msg: ShallowFBCSPCheckpoint ) -> None:
+        self.STATE.net = ShallowFBCSPNet.from_checkpoint( 
+            msg, device = self.SETTINGS.inference_device 
+        )
+
+    @ez.subscriber( INPUT_SIGNAL )
+    @ez.publisher( OUTPUT_DECODE )
+    async def decode_signal( self, msg: EEGMessage ) -> AsyncGenerator:
+        if self.STATE.net is not None:
+            if msg.n_time >= self.STATE.net.optimal_temporal_stride:
+                arr = np.swapaxes( msg.data, msg.time_dim, 0 )
+                output = self.STATE.net.inference( arr )
+                yield self.OUTPUT_DECODE, ClassDecodeMessage( data = output )
 
 
 # Dev/Test Fixture
@@ -124,9 +248,22 @@ class SampleSignalModulator( ez.Unit ):
         yield self.OUTPUT_EEG, sample
         yield self.OUTPUT_SAMPLE, replace( msg, sample = sample )
 
+class PublishOnceSettings( ez.Settings ):
+    msg: Any
+
+class PublishOnce( ez.Unit ):
+
+    SETTINGS: PublishOnceSettings
+
+    OUTPUT = ez.OutputStream( Any )
+
+    @ez.publisher( OUTPUT )
+    async def pub_once( self ) -> AsyncGenerator:
+        yield self.OUTPUT, self.SETTINGS.msg
+
 
 class ShallowFBCSPTrainingTestSystemSettings( ez.Settings ):
-    shallowfbcsptraining_settings: ShallowFBCSPTrainingSettings
+    shallowfbcsptraining_settings: FBCSPTrainingSettings
     trainingserver_settings: TrainingServerSettings
     eeg_settings: EEGSynthSettings = field( 
         default_factory = EEGSynthSettings 
@@ -149,8 +286,14 @@ class ShallowFBCSPTrainingTestSystem( ez.System ):
     PLOTTER = EEGPlotter()
 
     TRAIN_SERVER = TrainingServer()
-    FBCSP_TRAINING = ShallowFBCSPTraining()
+    FBCSP_TRAINING = FBCSPTraining()
     DEBUG = DebugLog()
+
+    PUBONCE = PublishOnce( 
+        PublishOnceSettings(
+            msg = Path( '.' ) / 'recordings'
+        )
+    )
 
     def configure( self ) -> None:
         self.FBCSP_TRAINING.apply_settings( 
@@ -193,9 +336,11 @@ class ShallowFBCSPTrainingTestSystem( ez.System ):
             ( self.EEG.OUTPUT_SIGNAL, self.PREPROC.INPUT_SIGNAL ),
             ( self.PREPROC.OUTPUT_SIGNAL, self.SAMPLER.INPUT_SIGNAL ),
             ( self.SAMPLER.OUTPUT_SAMPLE, self.INJECTOR.INPUT_SAMPLE ),
-            ( self.INJECTOR.OUTPUT_SAMPLE, self.FBCSP_TRAINING.INPUT_SAMPLE ),
+            # ( self.INJECTOR.OUTPUT_SAMPLE, self.FBCSP_TRAINING.INPUT_SAMPLE ),
+            ( self.INJECTOR.OUTPUT_SAMPLE, self.LOGGER.INPUT_MESSAGE ),
 
             ( self.INJECTOR.OUTPUT_SAMPLE, self.DEBUG.INPUT ),
+            ( self.PUBONCE.OUTPUT, self.FBCSP_TRAINING.INPUT_TRAIN ),
 
             ( self.TRAIN_SERVER.OUTPUT_SAMPLETRIGGER, self.SAMPLER.INPUT_TRIGGER ),
 
@@ -204,7 +349,7 @@ class ShallowFBCSPTrainingTestSystem( ez.System ):
             # ( self.WINDOW.OUTPUT_SIGNAL, self.PLOTTER.INPUT_SIGNAL ),
             ( self.INJECTOR.OUTPUT_EEG, self.PLOTTER.INPUT_SIGNAL ),
 
-            ( self.INJECTOR.OUTPUT_SAMPLE, self.LOGGER.INPUT_MESSAGE ),
+            
         )
 
     def process_components( self ) -> Tuple[ ez.Component, ... ]:
@@ -297,22 +442,10 @@ if __name__ == '__main__':
             ca_cert = cacert
         ),
 
-        shallowfbcsptraining_settings = ShallowFBCSPTrainingSettings(
-            model_spec = ShallowFBCSPParameters(
-                in_chans = channels,
-                n_classes = 2,
-                input_time_length = 1000
-            )
+        shallowfbcsptraining_settings = ModelTrainingSettings(
         )
     )
 
     system = ShallowFBCSPTrainingTestSystem( settings )
 
     ez.run_system( system )
-
-
-# Change TimeseriesMessage to TSMessage
-# Change EEGMessage to MultiChTSMessage
-# Alias EEGMessage
-
-# Template typing parameter for SampleMessage
